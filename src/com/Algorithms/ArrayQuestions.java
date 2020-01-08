57e8763fdbf2827e5b1d92b63cc2bb179a799c9a
Bindu Vasini <bindu.vasini@anaplan.com>
2020-01-07 21:07:05 -0800

    MergeInterval

--- src/com/Algorithms/ArrayQuestions.java
+++ src/com/Algorithms/ArrayQuestions.java
@@ -1,5 +1,7 @@
 package com.Algorithms;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class ArrayQuestions {
@@ -13,8 +15,29 @@ public class ArrayQuestions {
         return minHeap.remove();
     }
 
+    static void mergeIntervals(int[][] intervals) {
+        LinkedList<int[]> queue = new LinkedList<>();
+        //sort the value in 0th index of each array
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+
+        queue.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] currInterval = intervals[i];
+            int[] prevInterval = queue.getLast();
+
+            if (currInterval[0] > prevInterval[1]) { //if the current interval end point is already greater than the prev, we need it in the queue.
+                queue.addLast(currInterval);
+            }
+            queue.getLast()[1] = Math.max(prevInterval[1], currInterval[1]); //update the interval in the queue rather the current one.
+        }
+
+        for (int[] inters : queue) {
+            System.out.println(inters[0] + "," + inters[1]);
+        }
+    }
+
     public static void main(String[] args) {
-        int[] array = {3,2,3,1,2,4,9,5,5,6};
-        System.out.println(kthLargestElement(array, 4));
+        int[][] array = {{15, 18}, {1, 3}, {8, 10}, {2, 6}};
+        mergeIntervals(array);
     }
 }
