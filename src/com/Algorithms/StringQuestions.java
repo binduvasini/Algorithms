bbc86174aef4924b015442ead9b31a8b7377fb00
Bindu Vasini <bindu.vasini@anaplan.com>
2020-01-09 22:31:24 -0800

    String Regex matching

--- src/com/Algorithms/StringQuestions.java
+++ src/com/Algorithms/StringQuestions.java
@@ -69,4 +69,36 @@ public class StringQuestions {
         }
         return dp[n][m];
     }
+
+    boolean isRegexMatching(String s, String p) {
+        int n = s.length(), m = p.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        //both the pattern and string are empty
+        dp[0][0] = true;
+
+        //string is empty. the pattern is * so we look at the prev column value.
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p.charAt(j - 1) == '.' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
 }
