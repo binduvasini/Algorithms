f72cd39d340c9d53a04b6053c00d14a43838dfe1
binduvasini <bbalasubramanian@dons.usfca.edu>
2020-05-27 21:41:14 -0700

    k pairs with smallest sums

--- src/com/Algorithms/ArrayQuestions.java
+++ src/com/Algorithms/ArrayQuestions.java
@@ -452,4 +452,41 @@ public class ArrayQuestions {
         }
         return longestConsec;
     }
+
+    /**
+     * You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+     *
+     * Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+     *
+     * Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+     *
+     * Example 1:
+     *
+     * Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+     * Output: [[1,2],[1,4],[1,6]]
+     * Explanation: The first 3 pairs are returned from the sequence:
+     *              [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        if(nums1.length == 0 || nums2.length == 0)
+            return new LinkedList<>();
+        PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> (o.get(0) + o.get(1))));
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                minHeap.add(List.of(num1, num2));
+            }
+        }
+        List<List<Integer>> list = new LinkedList<>();
+        while(k > 0){
+            list.add(minHeap.remove());
+            if(minHeap.size() == 0)
+                break;
+            k -= 1;
+        }
+        return list;
+    }
 }
