941fa8f0ff1bc4c08e4a4b8950f64ef55b2823bb
binduvasini <bbalasubramanian@dons.usfca.edu>
2020-05-31 00:40:45 -0700

    edit distance

--- src/com/Algorithms/StringQuestions.java
+++ src/com/Algorithms/StringQuestions.java
@@ -417,4 +417,46 @@ public class StringQuestions {
         }
         return dp[A.length][B.length];
     }
+
+    /**
+     * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+     *
+     * You have the following 3 operations permitted on a word:
+     *
+     * Insert a character
+     * Delete a character
+     * Replace a character
+     *
+     * Input: word1 = "intention", word2 = "execution"
+     * Output: 5
+     * Explanation:
+     * intention -> inention (remove 't')
+     * inention -> enention (replace 'i' with 'e')
+     * enention -> exention (replace 'n' with 'x')
+     * exention -> exection (replace 'n' with 'c')
+     * exection -> execution (insert 'u')
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for(int i=0; i <= word1.length(); i++){
+            dp[i][0] = i;
+        }
+
+        for(int j=0; j <= word2.length(); j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1; i <= word1.length(); i++){
+            for(int j=1; j <= word2.length(); j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
 }
