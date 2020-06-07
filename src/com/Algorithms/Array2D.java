864d8cd903837a4ba8a94e68cbf4b55ef8dcd0c5
Bindu Vasini <bindu.vasini@anaplan.com>
2020-06-07 12:17:45 -0700

    add comments

--- src/com/Algorithms/Array2D.java
+++ src/com/Algorithms/Array2D.java
@@ -42,29 +42,23 @@ public class Array2D {
 
     /**
      * Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
-     * <p>
-     * Note that it is the kth smallest element in the sorted order, not the kth distinct element.
-     * <p>
-     * Example:
-     * <p>
      * matrix = [
      * [ 1,  5,  9],
      * [10, 11, 14],
      * [12, 14, 15]
      * ],
      * k = 8,
-     * <p>
      * return 14.
      *
      * @param matrix
      * @param k
      * @return
      */
-    public int kthSmallest(int[][] matrix, int k) {
+    public int kthSmallestElementInMatrix(int[][] matrix, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int[] cells : matrix) {
-            for (int cell : cells) {
-                maxHeap.add(cell);
+        for (int[] rows : matrix) {
+            for (int value : rows) {
+                maxHeap.add(value);
                 if (maxHeap.size() > k)
                     maxHeap.remove();
             }
@@ -74,17 +68,13 @@ public class Array2D {
 
     /**
      * There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
-     * <p>
      * Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
-     * <p>
      * Input: [[10,20],[30,200],[400,50],[30,20]]
      * Output: 110
-     * Explanation:
      * The first person goes to city A for a cost of 10.
      * The second person goes to city A for a cost of 30.
      * The third person goes to city B for a cost of 50.
      * The fourth person goes to city B for a cost of 20.
-     * <p>
      * The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
      *
      * @param costs
