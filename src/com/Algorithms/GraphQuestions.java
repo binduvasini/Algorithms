3cdc62f198fdadbe8273cd33b0228d3d4a13a8d3
Bindu Vasini <bindu.vasini@anaplan.com>
2020-06-07 12:07:16 -0700

    align

--- src/com/Algorithms/GraphQuestions.java
+++ src/com/Algorithms/GraphQuestions.java
@@ -35,9 +35,7 @@ public class GraphQuestions {
      * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
      * Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
      * Output: false
-     * There are a total of 2 courses to take.
-     * To take course 1 you should have finished course 0, and to take course 0 you should
-     * also have finished course 1. So it is impossible.
+     * There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
      */
 
 //    graph = new HashMap<>();
