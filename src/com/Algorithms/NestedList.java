89734d2106c6181aec5ba41512e1c5aac39288c1
binduvasini <bbalasubramanian@dons.usfca.edu>
2020-07-10 12:01:01 -0700

    flatten a nested list

--- src/com/Algorithms/NestedList.java
+++ src/com/Algorithms/NestedList.java
@@ -5,20 +5,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class NestedList {
-    public static void main(String[] args) {
-        List<Object> nestedList = lst(1, lst(2, lst(3, 4)), lst(5, 6, 7), 8, lst(lst(9, 10)), 9);
-        List<Integer> flatList = flatten(nestedList);
-        System.out.println(nestedList);
-        System.out.println(flatList);
+    public static List<Integer> flatten(List<?> nestedList) {
+        return flattenHelper(nestedList, new LinkedList<>());
     }
 
-    public static List<Integer> flatten(List<?> list) {
-        List<Integer> ret = new LinkedList<>();
-        flattenHelper(list, ret);
-        return ret;
-    }
-
-    public static void flattenHelper(List<?> nestedList, List<Integer> flatList) {
+    public static List<Integer> flattenHelper(List<?> nestedList, List<Integer> flatList) {
         for (Object item : nestedList) {
             if (item instanceof List<?>) {
                 flattenHelper((List<?>) item, flatList);
@@ -26,6 +17,14 @@ public class NestedList {
                 flatList.add((Integer) item);
             }
         }
+        return flatList;
+    }
+
+    public static void main(String[] args) {
+        List<Object> nestedList = lst(1, lst(2, lst(3, 4)), lst(5, 6, 7), 8, lst(lst(9, 10)), 11);
+        List<Integer> flatList = flatten(nestedList);
+        System.out.println("Nested list: "+nestedList);
+        System.out.println("Flattened list: "+flatList);
     }
 
     private static List<Object> lst(Object... objs) {
