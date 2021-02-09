1dfafc4c4483dbbdaaef3abbe0050403be6076cd
Bindu Vasini <bindu.vasini@anaplan.com>
2021-02-08 18:26:26 -0800

    Peeking Iterator

---
+++ src/com/Algorithms/PeekingIterator.java
@@ -0,0 +1,37 @@
+package com.Algorithms;
+
+import java.util.Iterator;
+
+public class PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> iterator;
+    Integer cache = null;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.iterator = iterator;
+        if (this.iterator.hasNext()) {
+            cache = this.iterator.next();
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return cache;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer nextElement = cache;
+        if (iterator.hasNext())
+            cache = iterator.next();
+        else
+            cache = null;
+        return nextElement;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cache != null;
+    }
+}
