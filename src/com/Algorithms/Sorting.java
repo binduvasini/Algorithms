85fd47f8ac16d233f1dcf4251e307d9fed3feb90
Bindu Vasini <bindu.vasini@anaplan.com>
2021-11-27 21:41:28 -0800

    sort

--- src/com/Algorithms/Sorting.java
+++ src/com/Algorithms/Sorting.java
@@ -1,8 +1,12 @@
 package com.Algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Sorting {
     /**
@@ -27,7 +31,9 @@ public class Sorting {
         //We need to sort the array in decreasing order because we need the minimum cost.
         // So rule out the maximum costs first.
         //If we sort it in increasing order instead, we will get the maximum cost.
-        Arrays.sort(costs, (c1, c2) -> Math.abs(c2[0] - c2[1]) - Math.abs(c1[0] - c1[1]));
+        Arrays.sort(costs, (c1, c2) ->
+                Math.abs(c2[0] - c2[1]) - Math.abs(c1[0] - c1[1])
+        );
 
         for (int[] cost : costs) {
             if ((cost[0] <= cost[1] && A < N) || B == N) {  //making sure we have N candidates in each city.
@@ -58,7 +64,7 @@ public class Sorting {
         // 1) their index position for the first few people will match k value.
         // 2) later when we insert everyone in the right position,
         //    we will not encounter ArrayIndexOutOfBounds exception.
-        Arrays.sort(people, (p1, p2) ->{
+        Arrays.sort(people, (p1, p2) -> {
             if (p1[0] == p2[0])
                 return p1[1] - p2[1];
             else
@@ -71,5 +77,78 @@ public class Sorting {
             queue.add(person[1], person);
         }
         return queue.toArray(new int[queue.size()][]);
+    }
+
+    /**
+     * Given an array of even length, check if it is possible to reorder elements such that
+     * the element and its double are next to each other.
+     * arr = [4,-2,2,-4]
+     * Output: true. Can be reordered as [-2,-4,2,4]
+     *
+     * @param arr
+     * @return
+     */
+    public boolean canReorderDoubled(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Integer[] array = Arrays.stream(arr).boxed().toArray( Integer[]::new ); //To use comparator
+        Arrays.sort(array, Comparator.comparingInt(Math::abs)); //To tackle negative numbers
+
+        for (int num : array) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : array) {
+            int doubled = num * 2;
+
+            if (map.get(num) == 0)  //No need to check whether map containsKey.
+                continue;
+            if (map.getOrDefault(doubled, 0) <= 0)  //Instead of containsKey
+                return false;
+
+            map.put(doubled, map.get(doubled) - 1);
+            map.put(num, map.get(num) - 1);
+
+        }
+        return true;
+    }
+
+    /**
+     * An array original is transformed into a doubled array changed by appending the double of every element,
+     * and then randomly shuffling the resulting array.
+     * Return the original array.
+     * changed = [1,3,4,2,6,8]
+     * Output: [1,3,4]
+     *
+     * @param changed
+     * @return
+     */
+    public int[] findOriginalArray(int[] changed) {
+        if (changed.length % 2 != 0)
+            return new int[0];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        Integer[] array = Arrays.stream(changed).boxed().toArray( Integer[]::new );
+        Arrays.sort(array, Comparator.comparingInt(Math::abs)); //To tackle negative numbers
+
+        for (int num : array) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : array) {
+            int doubled = num * 2;
+
+            if (map.get(num) == 0)
+                continue;
+            if (map.getOrDefault(doubled, 0) <= 0)
+                return new int[0];  //Negative case
+
+            map.put(doubled, map.get(doubled) - 1);
+            map.put(num, map.get(num) - 1);
+            list.add(num);
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
