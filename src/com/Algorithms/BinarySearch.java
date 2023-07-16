c43e02cc33f3cdbcf8c13c59569e4d22da66ec1f
bindu.vasini <bindu.vasini@anaplan.com>
2023-07-16 10:49:22 -0700

    BinarySearch

--- src/com/Algorithms/BinarySearch.java
+++ src/com/Algorithms/BinarySearch.java
@@ -253,4 +253,28 @@ public class BinarySearch {
 
         return nums[lo];
     }
+
+    /**
+     * A peak element is an element that is strictly greater than its neighbors.
+     * If the array contains multiple peaks, return the index to any of the peaks.
+     * @param nums
+     * @return
+     */
+    public int findPeakElement(int[] nums) {
+        int lo = 0, hi = nums.length - 1;
+
+        while(lo <= hi){
+            if(lo == hi)
+                return lo;
+
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] > nums[mid+1])
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+
+        return -1;
+    }
 }
