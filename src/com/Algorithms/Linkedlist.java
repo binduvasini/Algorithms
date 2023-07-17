cf8c6fef63c4b6b124917fee9f72e442846a5f7c
bindu.vasini <bindu.vasini@anaplan.com>
2023-07-16 19:49:01 -0700

    Linkedlist

--- src/com/Algorithms/Linkedlist.java
+++ src/com/Algorithms/Linkedlist.java
@@ -315,13 +315,16 @@ public class Linkedlist {
         dummy.next = head;
         Node first = dummy;
         Node second = dummy;
+
         for (int i = 1; i <= n + 1; i++) {
             first = first.next;
         }
+
         while (first != null) {
             first = first.next;
             second = second.next;
         }
+
         second.next = second.next.next;
         return dummy.next;
     }
