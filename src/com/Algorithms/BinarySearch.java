a672ef3846c252c3c8196c228419debc534a3a22
bindu.vasini <bindu.vasini@anaplan.com>
2023-07-16 19:45:56 -0700

    Linkedlist

--- src/com/Algorithms/BinarySearch.java
+++ src/com/Algorithms/BinarySearch.java
@@ -209,12 +209,12 @@ public class BinarySearch {
 
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if ((mid * mid) == num)
+            if (num == (mid * mid))
                 return true;
-            else if ((mid * mid) < num)
-                lo = mid + 1;
-            else
+            else if (num < (mid * mid))
                 hi = mid - 1;
+            else
+                lo = mid + 1;
         }
 
         return false;
@@ -233,10 +233,9 @@ public class BinarySearch {
         if (nums.length == 1)
             return nums[0];
 
-        int n = nums.length;
-        int lo = 0, hi = n - 1;
+        int lo = 0, hi = nums.length - 1;
 
-        while (lo < hi) {  //
+        while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             if (mid % 2 == 0) {
                 if (nums[mid] == nums[mid + 1])
--- src/com/Algorithms/Linkedlist.java
+++ src/com/Algorithms/Linkedlist.java
@@ -301,5 +301,29 @@ public class Linkedlist {
         }
         return newHead.next;
     }
+
+    /**
+     * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+     * Input:  1->2->3->4->5->6, n = 2
+     * Output: 1->2->3->4->6
+     * @param head
+     * @param n
+     * @return
+     */
+    public Node removeNthFromEnd(Node head, int n) {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node first = dummy;
+        Node second = dummy;
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
 }
 
