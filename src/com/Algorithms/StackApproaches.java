f83baab288fd9ef08a1f075a6b9994ec1be99e9e
bindu.vasini <bindu.vasini@anaplan.com>
2023-07-16 22:51:27 -0700

    Stack

--- src/com/Algorithms/StackApproaches.java
+++ src/com/Algorithms/StackApproaches.java
@@ -1,12 +1,15 @@
 package com.Algorithms;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.Map;
 
 public class StackApproaches {
 
     /**
-     * For every element in the array, find its next greater element print it in this value’s position or print -1. The Next greater element of a number is the first greater number to its right.
+     * For every element in the array, find its next greater element print it in this value’s position or print -1.
+     * The Next greater element of a number is the first greater number to its right.
      * Input: [4, 1, 6, 5, 9, 3, 2, 7]
      * Output: [6, 6, 9, 9, -1, 7, 7, -1]
      * @param nums
@@ -14,23 +17,31 @@ public class StackApproaches {
      */
     public int[] nextGreaterElementsWithoutDuplicates(int[] nums) {
         int[] nextGreaterElements = new int[nums.length];
-        ArrayDeque<Integer> stack = new ArrayDeque<>();  //Use ArrayDeque class which will at least restrict the access to one of the ends, instead of Stack (that offers search & elementAt)
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();  //Use ArrayDeque class which will
+        // restrict the access to one of the ends, instead of Stack (that offers search & elementAt)
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int num : nums) {
             while (!stack.isEmpty() && num >= stack.peek()) {
                 map.put(stack.pop(), num);
             }
             stack.push(num);
         }
+
         for (int i = 0; i < nums.length; i++) {
             nextGreaterElements[i] = map.getOrDefault(nums[i], -1);
         }
+
         return nextGreaterElements;
     }
 
     /**
-     * Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
-     *
+     * Given a circular array (the next element of the last element is the first element of the array),
+     * print the Next Greater Number for every element.
+     * The Next Greater Number of a number x is the first greater number to its traversing-order next in the array,
+     * which means you could search circularly to find its next greater number.
+     * If it doesn't exist, output -1 for this number.
+
      * Example 1:
      * Input: [1,2,1]
      * Output: [2,-1,2]
@@ -42,9 +53,10 @@ public class StackApproaches {
      */
     public int[] nextGreaterElementsWithDuplicates(int[] nums) {
         int[] nextGreaterElements = new int[nums.length];
-        ArrayDeque<int[]> stack = new ArrayDeque<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Deque<int[]> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
+
         for (int i = 0; i < 2 * n; i++) {
             int num = nums[i % n];
             while (!stack.isEmpty() && num > stack.peek()[1]) {
@@ -52,26 +64,56 @@ public class StackApproaches {
             }
             stack.push(new int[]{i, num});
         }
+
         for (int i = 0; i < n; i++) {
             nextGreaterElements[i] = map.getOrDefault(i, -1);
         }
+
         return nextGreaterElements;
+    }
+
+    /**
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+     * determine if the input string is valid.
+
+     * An input string is valid if:
+     * Open brackets are closed by the same type of brackets.
+     * Open brackets are closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     * @param s
+     * @return
+     */
+    public boolean isValidParentheses(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : s.toCharArray())
+        {
+            if(c == '(')
+                stack.push(')');
+            else if(c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
     }
 
     /**
      * Given a string containing only three types of characters: '(', ')'
      * and '*', write a function to check whether this string is valid.
      * We define the validity of a string by these rules:
-     * 1. Any left parenthesis '(' must have a corresponding right parenthesis ')'.
-     * 2. Any right parenthesis ')' must have a corresponding left parenthesis '('.
-     * 3. Left parenthesis '(' must go before the corresponding right parenthesis ')'.
-     * 4. '*' could be treated as a single right parenthesis ')' 
-     * or a single left parenthesis '(' or an empty string.
+     * 1. Any left parenthesis'('must have a corresponding right parenthesis')'.
+     * 2. Any right parenthesis')'must have a corresponding left parenthesis'('.
+     * 3. Left parenthesis'('must go before the corresponding right parenthesis')'.
+     * 4. '*'could be treated as a single right parenthesis')'
+     * or a single left parenthesis'('or an empty string.
      * 5. An empty string is also valid.
      */
-    public boolean checkValidString(String s) {
-        ArrayDeque<Integer> openStack = new ArrayDeque<>();
-        ArrayDeque<Integer> starStack = new ArrayDeque<>();
+    public boolean checkValidParenthesesWithWildcard(String s) {
+        Deque<Integer> openStack = new ArrayDeque<>();
+        Deque<Integer> starStack = new ArrayDeque<>();
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(')
@@ -87,10 +129,12 @@ public class StackApproaches {
                     return false;
             }
         }
+
         while (!openStack.isEmpty() && !starStack.isEmpty()) {
             if (openStack.pop() > starStack.pop())
                 return false;
         }
+
         return openStack.isEmpty();
     }
 
@@ -107,7 +151,7 @@ public class StackApproaches {
      */
     public int[] dailyTemperatures(int[] T) {
         int[] output = new int[T.length];
-        ArrayDeque<int[]> stack = new ArrayDeque<>();  //store the index and value as a pair.
+        Deque<int[]> stack = new ArrayDeque<>();  //store the index and value as a pair.
 
         for (int i = 0; i < T.length; i++) {
             int currTemp = T[i];
@@ -126,15 +170,19 @@ public class StackApproaches {
     /**
      * StockSpanner collects the daily price quotes for a stock and
      * returns the span of that stock's price for the current day.
-     * The span of the stock's price today is defined as the maximum number of previous days
-     * for which the price of the stock was less than or equal to today's price.
-     * For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 58, 75, 85],
-     * then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+
+     * The span of the stock's price in one day is the maximum number of consecutive days
+     * (starting from that day and going backward)
+     * for which the stock price was less than or equal to the price of that day.
+
+     * Example: If the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8,
+     * then the span of today is 3
+     * because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
      *
      * @param price
      * @return
      */
-    ArrayDeque<int[]> stocks = new ArrayDeque<>();  //Store the spanValue and the current price as a pair.
+    Deque<int[]> stocks = new ArrayDeque<>();  //Store the spanValue and the current price as a pair.
 
     public int stockSpanner(int price) {
         int spanOfStock = 1;
